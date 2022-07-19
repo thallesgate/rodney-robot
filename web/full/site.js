765c08b9cb5d7c25e602cfce1c86697337fff2f6
Thalles Candido da Silva <39136484+thallesgate@users.noreply.github.com>
2022-07-19 19:43:51 -0300

    update

--- web/full/site.js
+++ web/full/site.js
@@ -1,7 +1,27 @@
 // Robot Information
-var ip_address = '192.168.1.200'; //Robot IP
+var ip_address = '192.168.1.100'; //Robot IP
 var camera_topic = '/camera/rgb/image_rect_color'; //This is to allow us to change between /camera/rgb/image_rect_color and /camera/ir/image_rect_ir
-var occupancygrid_topic = '/rtabmap/grid_map'; ///rtabmap/octomap_grid grid_map odom_local_map
+var occupancygrid_topic = '/map'; // '/rtabmap/grid_map'; ///rtabmap/octomap_grid grid_map odom_local_map
+/*
+var videoFeedWidthPercentage = 35
+var videoFeedHeightPercentage = 40;
+
+var mapWidthPercentage = 35;
+var mapHeightPercentage = 40;
+
+
+var videoFeedWidth = (window.innerWidth * videoFeedWidthPercentage)/100;
+var videoFeedHeight = (window.innerHeight * videoFeedHeightPercentage)/100;
+var videoFeedInterval = 200;
+
+var mapWidth = (window.innerWidth * mapWidthPercentage)/100 ;
+var mapHeight = (window.innerHeight * mapHeightPercentage)/100;
+*/
+var videoFeedWidth = 540;
+var videoFeedHeight = 380;
+var videoFeedInterval = 200;
+var mapWidth = 540;
+var mapHeight = 380;
 
 //ROS Connectivity
 //ROS: Initializing ROS Library
@@ -23,34 +43,35 @@ var occupancygrid_topic = '/rtabmap/grid_map'; ///rtabmap/octomap_grid grid_map 
   });
 
 //Main method to initialize viewers.
-function init(){
-    
+function initViewers(){
+
   //MJPEG library function
-  var viewer = new MJPEGCANVAS.Viewer({
-    divID : 'mjpeg', //div for viewer generation
+  var streamViewer = new MJPEGCANVAS.Viewer({
+    divID : 'videoFeed', //div for viewer generation
     host : ip_address,
-    width : 540,
-    height : 380,
+    width : videoFeedWidth,
+    height : videoFeedHeight,
     topic : camera_topic,
-    interval : 200
+    interval : videoFeedInterval
   });
 
   //ROS2D Map Viewer
-  var viewer = new ROS2D.Viewer({
+  var mapViewer = new ROS2D.Viewer({
     divID : 'map', //same happens here
-    width : 540,
-    height : 380, 
+    width : mapWidth,
+    height : mapHeight  
   });
 
   var gridClient = new ROS2D.OccupancyGridClient({
     ros : ros,
-    rootObject : viewer.scene,
+    rootObject : mapViewer.scene,
     topic : occupancygrid_topic,
     continuous : true,
   });
 
   gridClient.on('change', function(){
-    viewer.scaleToDimensions(gridClient.currentGrid.width, gridClient.currentGrid.height);
-    viewer.shift(gridClient.currentGrid.pose.position.x, gridClient.currentGrid.pose.position.y);
+    mapViewer.scaleToDimensions(gridClient.currentGrid.width, gridClient.currentGrid.height);
+    mapViewer.shift(gridClient.currentGrid.pose.position.x, gridClient.currentGrid.pose.position.y);
   });
+
 }
