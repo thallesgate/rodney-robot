ac8c3274671a5fd1cbc493b6473f9069944c1765
thallesgate <thallesgate@gmail.com>
2022-07-24 04:11:08 -0300

    Update rosfunctions.js

--- web/full/scripts/rosfunctions.js
+++ web/full/scripts/rosfunctions.js
@@ -1,5 +1,5 @@
 // Robot Information
-var ip_address = '177.209.123.253'; //Robot IP
+var ip_address = '192.168.1.100'; //Robot IP
 var camera_topic = '/camera/rgb/image_color'; //This is to allow us to change between /camera/rgb/image_rect_color and /camera/ir/image_rect_ir
 var occupancygrid_topic = '/map'; //RTABMAP MAP: '/rtabmap/grid_map'; OR /rtabmap/octomap_grid OR grid_map OR odom_local_map
 
